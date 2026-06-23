754e1ae730be712d22d6bb8048560bf4c01e4b17
DHANYA S <dhanyas24cs@srishakthi.ac.in>
2026-06-23 21:15:52 +0530

    Add Problem 11: Container With Most Water

---
+++ Problem: 11. Container With Most Water.java
@@ -0,0 +1,30 @@
+
+Problem: 11. Container With Most Water
+
+
+class Solution {
+
+    public int maxArea(int[] height) {
+
+        int l = 0;
+        int r = height.length - 1;
+
+        int max = 0;
+
+        while (l < r) {
+
+            max = Math.max(
+                max,
+                Math.min(height[l], height[r]) * (r - l)
+            );
+
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return max;
+    }
+}
